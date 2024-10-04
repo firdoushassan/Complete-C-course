4ca08ede9b286b9c140eb9631e70e6715aa5a95f
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-05 00:55:20 +0530

    Chapter 4 uploaded

---
+++ Chapter 4/tempCodeRunnerFile.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, prime = 1;
+    printf("enter the number\t", n);
+    scanf("%d", &n);
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            prime = 0;
+            break;
+        }
+    }
+    if (prime = 0 && n != 2)
+    {
+        printf("The number is not prime");
+    }
+    else
+    {
+        printf("This is a prime number");
+    }
+    return 0;
+}
