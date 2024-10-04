9263bec8c4d890b853dc0bec803d25091f540629
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-04 14:47:18 +0530

    Chapter 2 uploaded

---
+++ Chapter 2/arithmetic_instructions.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a = 4;
+    int b = 8;
+    printf("The value of a + b is : %d\n", a + b);
+    printf("The value of a * b is : %d\n", a * b);
+    printf("The value of a / b is : %d\n", a / b);
+    printf("The value of a - b is : %d\n", a - b);
+    printf("The value of 4 to the power of 5 is %f\n", pow(4, 5));
+    return 0;
+}
