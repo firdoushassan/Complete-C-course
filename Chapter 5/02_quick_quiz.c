a7428eed4a38ff743add05fcbe9885de4db18bc1
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-06 00:30:16 +0530

    Add files via upload

---
+++ Chapter 5/02_quick_quiz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void GoodMorning();
+void GoodAfternoon();
+void GoodNight();
+int main()
+{
+    GoodMorning();
+    GoodAfternoon();
+    GoodNight();
+
+    return 0;
+}
+
+void GoodMorning()
+{
+    printf("Good Morning by C\n");
+}
+void GoodAfternoon()
+{
+    printf("Good Afternoon by C\n");
+}
+void GoodNight()
+{
+    printf("Good Night by C\n");
+}
