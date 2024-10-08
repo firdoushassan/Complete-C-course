0ec7441f45966419963c177200b81f7303e3f710
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-09 00:17:56 +0530

    Add files via upload

---
+++ Chapter 7/04_pointer_arithmetic.c
@@ -0,0 +1,31 @@
+# include <stdio.h>
+
+int main(){
+    // int i = 34;
+    // int *ptr = &i;
+    // printf("The value of ptr is %u\n", ptr);
+    // ptr--;
+    // //  ptr++;
+    // // ptr++;
+    // printf("The value of ptr is %u\n", ptr);
+
+
+    //    char i = 'A';
+    // char *ptr = &i;
+    // printf("The value of ptr is %u\n", ptr);
+    // ptr++;
+    // //  ptr++;
+    // // ptr++;
+    // printf("The value of ptr is %u\n", ptr);
+
+
+     float f = 4.6;
+    float *ptr = &f;
+    printf("The value of ptr is %u\n", ptr);
+    ptr++;
+    //  ptr++;
+    // ptr++;
+    printf("The value of ptr is %u\n", ptr);
+    
+return 0;
+}
