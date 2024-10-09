a067719ad56572de79981119c351e981a677d46f
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-10 00:48:26 +0530

    Add files via upload

---
+++ Chapter 8/03_strlen.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char *st = "hello";
+    int a = strlen(st); //Tells the number of words in the string
+    printf("The length of the string is %d", a);
+    return 0;
+}
