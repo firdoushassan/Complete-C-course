a1256bcfd21c1258300d0a9a472b3a9f8fcbc284
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-10 00:48:08 +0530

    Add files via upload

---
+++ Chapter 8/02_gets_puts.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main(){
+     char s[20];
+    printf("Enter the string \n", s);
+    gets(s); //used to scan it(multiple words)
+    puts(s); //used to print it
+    // printf("Your entered string is %s", s);
+return 0;
+}
