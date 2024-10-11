4a279f2ec032154748b437aef9194337386d1b1d
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-10-12 03:05:51 +0530

    Add files via upload

---
+++ Chapter 10/01_file_basics.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+
+int main(){
+    FILE *ptr;
+    ptr = fopen("sample.txt", "r"); //For reading the file
+    ptr = fopen("sample.txt", "w"); //For writing in the file
+
+return 0;
+}
